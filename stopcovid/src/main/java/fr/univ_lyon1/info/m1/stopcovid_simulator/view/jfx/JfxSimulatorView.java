c87aee640de0bd58e93e702a590e1cd918e67973
Alexis Plaquet <alexis.plaquet@etu.univ-lyon1.fr>
2020-09-19 00:50:44 +0200

    JfxSimulatorView updates userviews's foreign keys on model added user

--- stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/view/jfx/JfxSimulatorView.java
+++ stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/view/jfx/JfxSimulatorView.java
@@ -13,6 +13,11 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class JfxSimulatorView extends VBox implements SimulatorView {
     private static final int WIDTH = 600;
     private static final int HEIGHT = 600;
@@ -22,6 +27,7 @@ public class JfxSimulatorView extends VBox implements SimulatorView {
     private Observable addUserObservable = new Observable();
 
     private VBox userViewsVbox;
+    private Map<String, UserDebugView> userViews = new HashMap<>();
 
     private ServerModel serverModel;
     private SimulatorModel simulatorModel;
@@ -58,7 +64,7 @@ public class JfxSimulatorView extends VBox implements SimulatorView {
         Button testAddButton = new Button("ADD USER");
         testAddButton.setOnMouseClicked(event -> addUserObservable.emit());
 
-        root.getChildren().addAll(usersScrollpane, testAddButton);
+        root.getChildren().addAll(testAddButton, usersScrollpane);
 
         final Scene scene = new Scene(root, WIDTH, HEIGHT);
         stage.setScene(scene);
@@ -80,5 +86,20 @@ public class JfxSimulatorView extends VBox implements SimulatorView {
         UserLocalModel user = simulatorModel.getUser(token);
         JfxUserView userView = new JfxUserView(user);
         userViewsVbox.getChildren().add(userView.getRoot());
+        userViews.put(token, userView);
+
+        List<UserLocalModel> usersList = simulatorModel.getUsers();
+        for (UserLocalModel uModel : usersList) {
+            UserDebugView uView = userViews.get(uModel.getUserToken());
+            uView.updateVisibleForeignKeys(simulatorModel.getUsers()
+                    .stream()
+                    .map(userLocalModel -> userLocalModel.
+                            getPersonalKeysManager().
+                            getNewestKey().
+                            getKey())
+                    .collect(Collectors.toList()));
+        }
+
+
     }
 }
