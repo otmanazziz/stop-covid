2a5b031b707a2d6d48659e32b71bd6534d3473c0
Alexis Plaquet <alexis.plaquet@etu.univ-lyon1.fr>
2020-09-19 23:31:38 +0200

    add flagging strategy selection to RegularActionsPane

--- stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/view/jfx/user/RegularActionsPane.java
+++ stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/view/jfx/user/RegularActionsPane.java
@@ -1,11 +1,14 @@
 package fr.univ_lyon1.info.m1.stopcovid_simulator.view.jfx.user;
 
+import fr.univ_lyon1.info.m1.stopcovid_simulator.model.local.user.ContactAmountRiskyFlagging;
+import fr.univ_lyon1.info.m1.stopcovid_simulator.model.local.user.RiskyFlaggingStrategy;
 import fr.univ_lyon1.info.m1.stopcovid_simulator.util.Observable;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
@@ -13,9 +16,25 @@ import javafx.scene.layout.VBox;
 
 public class RegularActionsPane extends TitledPane {
 
+    private enum RiskyStrategiesEnum {
+        COUNT_1("1 contact", new ContactAmountRiskyFlagging(1)),
+        COUNT_2("2 contacts", new ContactAmountRiskyFlagging(2)),
+        COUNT_3("3 contacts", new ContactAmountRiskyFlagging(3));
+
+        private final String prettyName;
+        private final RiskyFlaggingStrategy strategy;
+
+        RiskyStrategiesEnum(final String prettyName, final RiskyFlaggingStrategy strategy) {
+            this.prettyName = prettyName;
+            this.strategy = strategy;
+        }
+    }
+
     private Observable declaredInfected = new Observable();
+    private Observable riskyStrategyChanged = new Observable();
 
     private TextField covidTokenField;
+    private ComboBox<String> strategyComboBox;
 
     /**
      * Creates a RegularActionPane.
@@ -25,7 +44,10 @@ public class RegularActionsPane extends TitledPane {
         regularActionsPane.setText("Actions normales");
         regularActionsPane.setExpanded(false);
 
-        VBox regularActionsVbox = new VBox();
+        HBox hbox = new HBox();
+
+        // Declare infected Vbox
+        VBox declareInfectedVbox = new VBox();
 
         HBox covidToken = new HBox();
         covidToken.setAlignment(Pos.CENTER_LEFT);
@@ -39,8 +61,22 @@ public class RegularActionsPane extends TitledPane {
         declareInfectedButton.setMaxWidth(Double.MAX_VALUE);
         declareInfectedButton.setOnAction(event -> declaredInfected.emit());
 
-        regularActionsVbox.getChildren().addAll(covidToken, declareInfectedButton);
-        regularActionsPane.setContent(regularActionsVbox);
+        declareInfectedVbox.getChildren().addAll(covidToken, declareInfectedButton);
+
+        // Risky flagging strategy vbox
+        VBox riskyFlaggingVbox = new VBox();
+        Label riskyFlaggingTitle = new Label("Stratégie de détection de risque");
+        strategyComboBox = new ComboBox<>();
+        strategyComboBox.valueProperty().addListener(observable -> riskyStrategyChanged.emit());
+        for (RiskyStrategiesEnum rs : RiskyStrategiesEnum.values()) {
+            strategyComboBox.getItems().add(rs.prettyName);
+        }
+
+        riskyFlaggingVbox.getChildren().addAll(riskyFlaggingTitle, strategyComboBox);
+
+        //
+        hbox.getChildren().addAll(declareInfectedVbox, riskyFlaggingVbox);
+        regularActionsPane.setContent(hbox);
     }
 
 
@@ -48,8 +84,22 @@ public class RegularActionsPane extends TitledPane {
         return declaredInfected;
     }
 
+    public Observable getRiskyStrategyChanged() {
+        return riskyStrategyChanged;
+    }
+
     public String getCovidToken() {
         return covidTokenField.getText();
     }
 
+    /**
+     * Retrieve the currently selected {@link RiskyFlaggingStrategy}.
+     * @return
+     */
+    public RiskyFlaggingStrategy getSelectedRiskyStrategy() {
+        RiskyFlaggingStrategy rs;
+        int selectedIndex = strategyComboBox.getSelectionModel().getSelectedIndex();
+        RiskyStrategiesEnum selectedEnum = RiskyStrategiesEnum.values()[selectedIndex];
+        return selectedEnum.strategy;
+    }
 }
